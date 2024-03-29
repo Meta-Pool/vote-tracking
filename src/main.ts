f4cc358fff6ebc3cb7cceca008d2101cc06ea73b
the-preem-palver <the.Preem.Palver@protonmail.com>
2024-03-29 14:31:01 -0300

    add unlocking trenches

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { writeFileSync } from "fs";
 import { MetaVoteContract } from "./meta-vote";
 import { setRpcUrl, yton } from "near-api-lite";
 import { argv, cwd } from "process";
+import { setGlobalDryRunMode } from "./base-smart-contract";
 
 type ByContractInfoType = {
     contract: string;
@@ -15,6 +16,15 @@ type MetaVoteDataType = {
     totalVotingPower: number;
     totalVotingPowerUsed: number;
     votesPerAddress: ByContractInfoType[];
+    totalUnlocking7dOrLess: number;
+    totalUnlocking15dOrLess: number;
+    totalUnlocking30dOrLess: number;
+    totalUnlocking60dOrLess: number;
+    totalUnlocking90dOrLess: number;
+    totalUnlocking120dOrLess: number;
+    totalUnlocking180dOrLess: number;
+    totalUnlocking240dOrLess: number;
+    totalUnlockingMores240d: number;
 }
 
 async function processMetaVote(): Promise<MetaVoteDataType> {
@@ -27,6 +37,21 @@ async function processMetaVote(): Promise<MetaVoteDataType> {
     let totalVotingPower = 0
     let totalVotingPowerUsed = 0
     let votesPerAddress: ByContractInfoType[] = []
+
+    //const E24 = BigInt("1" + "0".repeat(24))
+    //const E6 = BigInt("1" + "0".repeat(6))
+    let totalUnlocking7dOrLess = 0
+    let totalUnlocking15dOrLess = 0
+    let totalUnlocking30dOrLess = 0
+    let totalUnlocking60dOrLess = 0
+    let totalUnlocking90dOrLess = 0
+    let totalUnlocking120dOrLess = 0
+    let totalUnlocking180dOrLess = 0
+    let totalUnlocking240dOrLess = 0
+    let totalUnlockingMores240d = 0
+
+    console.log("processMetaVote, allVoters.length:", allVoters.length)
+
     for (let voter of allVoters) {
         if (!voter.locking_positions) continue;
 
@@ -39,9 +64,34 @@ async function processMetaVote(): Promise<MetaVoteDataType> {
                 userTotalVotingPower += yton(lp.voting_power)
             }
             else if (lp.is_unlocked) {
-                totalUnlocking += metaAmount
+                totalUnlocked += metaAmount
             }
-            else totalUnlocked += metaAmount;
+            else {
+                totalUnlocking += metaAmount;
+                //hasLockedOrUnlocking = true
+                const unixMsTimeNow = new Date().getTime()
+                const unlockingEndsAt = (lp.unlocking_started_at || 0) + lp.locking_period * 24 * 60 * 60 * 1000
+                const remainingDays = Math.trunc((unlockingEndsAt - unixMsTimeNow) / 1000 / 60 / 60 / 24)
+                if (remainingDays <= 7) {
+                    totalUnlocking7dOrLess += metaAmount
+                } else if (remainingDays <= 15) {
+                    totalUnlocking15dOrLess += metaAmount
+                } else if (remainingDays <= 30) {
+                    totalUnlocking30dOrLess += metaAmount
+                } else if (remainingDays <= 60) {
+                    totalUnlocking60dOrLess += metaAmount
+                } else if (remainingDays <= 90) {
+                    totalUnlocking90dOrLess += metaAmount
+                } else if (remainingDays <= 120) {
+                    totalUnlocking120dOrLess += metaAmount
+                } else if (remainingDays <= 180) {
+                    totalUnlocking180dOrLess += metaAmount
+                } else if (remainingDays <= 240) {
+                    totalUnlocking240dOrLess += metaAmount
+                } else {
+                    totalUnlockingMores240d += metaAmount
+                }
+            }
         }
 
         totalLocked += userTotalMetaLocked
@@ -81,7 +131,17 @@ async function processMetaVote(): Promise<MetaVoteDataType> {
         totalUnLocked: totalUnlocked,
         totalVotingPower: totalVotingPower,
         totalVotingPowerUsed: totalVotingPowerUsed,
-        votesPerAddress: votesPerAddress
+        votesPerAddress: votesPerAddress,
+
+        totalUnlocking7dOrLess,
+        totalUnlocking15dOrLess,
+        totalUnlocking30dOrLess,
+        totalUnlocking60dOrLess,
+        totalUnlocking90dOrLess,
+        totalUnlocking120dOrLess,
+        totalUnlocking180dOrLess,
+        totalUnlocking240dOrLess,
+        totalUnlockingMores240d
     }
 
 }
@@ -95,6 +155,12 @@ async function process() {
 
 export const useTestnet = false; //argv.includes("test") || argv.includes("testnet") || cwd().includes("testnet");
 export const useMainnet = !useTestnet
-const META_VOTE_CONTRACT_ID = useMainnet ? "meta-vote.near" : "metavote.testnet"
+export const dryRun = argv.includes("dry")
+if (dryRun) setGlobalDryRunMode(true)
+
+if (useTestnet) console.log("USING TESTNET")
+export const META_VOTE_CONTRACT_ID = useMainnet ? "meta-vote.near" : "metavote.testnet"
+export const META_PIPELINE_CONTRACT_ID = useMainnet ? "meta-pipeline.near" : "dev-1686255629935-21712092475027"
+export const META_PIPELINE_OPERATOR_ID = useMainnet ? "pipeline-operator.near" : "meta-vote.testnet"
 if (useTestnet) setRpcUrl("https://rpc.testnet.near.org")
 process()
